fe9625973ec84d1d903bf29398c83131fec14d9d
dnorth98 <dnorth98@gmail.com>
2014-02-10 16:41:46 -0500

    Added syslog and better FQDN handling
    Added in logging to syslog and also handling the fact that a hostname is not an FQDN

--- updatedns.py
+++ updatedns.py
@@ -33,35 +33,52 @@ import re
 import socket
 import sys
 import urllib
+import syslog
+import os
 
 from boto.route53.connection import Route53Connection
 from boto.route53.record import ResourceRecordSets
 from boto.route53.exception import DNSServerError
 
-# Find hostname from system if not specified on command line
+def myLog(message):
+    procName = __file__
+    syslogMsg = procName + ": " + message
+    syslog.syslog(syslogMsg)
+    print '%s' % message
+
+# get the FQDN
+# the domain is specified on the command line, hostname we get from the system
 if len(sys.argv) == 2:
-    hostname = sys.argv[1]
-else:
     from socket import gethostname
     hostname = gethostname()
+    domainname = sys.argv[1]
+
+    if domainname[0] != ".":
+        domainname = "." + domainname
+
+    fqdn = hostname + domainname
+
+    myLog("FQDN: %s" % fqdn)
+else:
+    myLog("No domain component specified on command line")
+    sys.exit(1)
 
 try:
     hostname
 except NameError:
-    print 'Hostname not specified and not able to detect.'
+    myLog("Unable to read hostname")
     sys.exit(1)
 
 # Add trailing dot to hostname if it doesn't have one
-if hostname[-1:] != ".":
-    hostname += "."
-
-print 'Hostname: %s' % hostname
+if fqdn[-1:] != ".":
+    fqdn += "."
 
 # Get current IP
 try:
+    myLog("Getting public IP from instance metadata service")
     ipsite = urllib.urlopen('http://169.254.169.254/latest/meta-data/public-ipv4')
 except:
-    print 'Connection error getting IP address.'
+    myLog("Connection error getting IP address")
     sys.exit(1)
 
 response = ipsite.read()
@@ -72,17 +89,17 @@ if type(ips) in [list,  tuple,  set]:
 try:
     newip
 except NameError:
-    print 'Unable to find IP address in response from check site.'
+    myLog("Unable to find IP address in response from check site")
     sys.exit(1)
 
 # Verify it's a good ip
 try:
     socket.inet_aton(newip)
 except socket.error:
-    print 'Received invalid IP address.'
+    myLog("Received invalid IP address")
     sys.exit(1)
 
-print 'Current IP: %s' % newip
+myLog("Current Public IP: %s" % newip)
 
 # Initialize the connection to AWS Route53
 if awskeyid and awskeysecret:
@@ -94,60 +111,58 @@ else:
 try:
     route53zones = route53.get_all_hosted_zones()
 except DNSServerError,  e:
-    print 'Connection error to AWS. Check your credentials.'
-    print 'Error %s - %s' % (e.code,  str(e))
+    myLog("Connection error to AWS. Check your credentials")
+    myLog("Error %s - %s" % (e.code,  str(e)))
     sys.exit(1)
 
 for zone in route53zones['ListHostedZonesResponse']['HostedZones']:
-    if zone['Name'][0:-1] in hostname:
+    if zone['Name'][0:-1] in fqdn:
         zoneid = zone['Id'].replace('/hostedzone/', '')
-        print 'Found Route53 Zone %s for hostname %s' % (zoneid,  hostname)
+        myLog("Found Route53 Zone %s for fqdn %s" % (zoneid,  fqdn))
 
 try:
     zoneid
 except NameError:
-    print 'Unable to find Route53 Zone for %s' % hostname
+    myLog("Unable to find Route53 Zone for %s" % fqdn)
     sys.exit(1)
 
 # Find the old record if it exists
 try:
     sets = route53.get_all_rrsets(zoneid)
 except DNSServerError,  e:
-    print 'Connection error to AWS.'
-    print 'Error %s - %s' % (e.code,  str(e))
+    myLog("Connection error to AWS")
+    myLog("Error %s - %s" % (e.code,  str(e)))
     sys.exit(1)
 
 for rset in sets:
-    if rset.name == hostname and rset.type == 'A':
+    if rset.name == fqdn and rset.type == 'A':
         curiprecord = rset.resource_records
         if type(curiprecord) in [list, tuple, set]:
             for record in curiprecord:
                 curip = record
-        print 'Current DNS IP: %s' % curip
+        myLog("Current DNS IP: %s" % curip)
         curttl = rset.ttl
-        print 'Current DNS TTL: %s' % curttl
+        myLog("Current DNS TTL: %s" % curttl)
 
         if curip != newip:
             # Remove the old record
-            print 'Removing old record...'
+            myLog("Removing old record from Route53...")
             change1 = ResourceRecordSets(route53, zoneid)
-            removeold = change1.add_change("DELETE", hostname, "A", curttl)
+            removeold = change1.add_change("DELETE", fqdn, "A", curttl)
             removeold.add_value(curip)
             change1.commit()
         else:
-            print 'IPs match,  not making any changes in DNS.'
+            myLog("IPs match in DNS, no DNS changes being made")
             sys.exit(0)
 
 try:
     curip
 except NameError:
-    print 'Hostname %s not found in current zone record' % hostname
-
+    myLog("FQDN %s not found in current zone record" % fqdn)
 
 # Add the new record
-print 'Adding %s to DNS as %s...' % ( hostname,  newip)
+myLog("Adding %s to DNS as %s..." % ( fqdn,  newip))
 change2 = ResourceRecordSets(route53, zoneid)
-change = change2.add_change("CREATE", hostname, "A", 60)
+change = change2.add_change("CREATE", fqdn, "A", 60)
 change.add_value(newip)
 change2.commit()
-
